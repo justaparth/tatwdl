2ba32bc7ed3a305318767e6400d0624d63284bc4
Parth Upadhyay <parth.upadhyay@gmail.com>
2011-11-06 10:38:56 -0600

    Added some comments

--- tatwdl.py
+++ tatwdl.py
@@ -14,8 +14,9 @@ def usage():
     sys.exit(1)
 
 def main():
-    argc = len(sys.argv)
     
+    # Verify arguments
+    argc = len(sys.argv)
     if argc < 2 or argc > 3:
         usage()
     start = int(sys.argv[1])
@@ -23,14 +24,17 @@ def main():
         end = start
     else:
         end = int(sys.argv[2])
-
     if start > end:
         usage()
+    
+    # Loop starting from 'start'
     dlstring = "http://tatw-archives.anjunabeats.com/TATW{0}.mp3".format(start)
     while start <= end:
+        # Subprocess call to wget
         subprocess.call(["wget", dlstring])
         start = start + 1
         dlstring = "http://tatw-archives.anjunabeats.com/TATW{0}.mp3".format(start)
+
 if __name__ == "__main__":
     main()
 
